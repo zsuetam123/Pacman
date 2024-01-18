79b038cf9b4aeb64b8acaeb5195387cd1a2b0c6d
mateu <mateu@DESKTOP-FSCLUIK.home>
2024-01-18 14:39:37 +0100

    First commit

---
+++ PacmanGame/desktop/src/com/pacman/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pacman.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pacman.game.PacmanGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Pacman-game");
+		new Lwjgl3Application(new PacmanGame(), config);
+	}
+}
